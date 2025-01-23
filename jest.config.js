88b6f90c0d4244c585df9fe55721f8dda98d1738
kavinphan <phan.kavin@gmail.com>
2025-01-23 16:14:11 -0500

    Only use Babel for Jest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} **/
+/** @type {import("ts-jest").JestConfigWithTsJest} **/
 module.exports = {
   testEnvironment: "node",
   preset: "ts-jest",
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
+    "^@/(.*)$": "<rootDir>/src/$1",
   },
   setupFilesAfterEnv: ["./src/test/dbMock.ts", "./src/test/authMock.ts"],
   transform: {
-    '^.+\\.(ts|tsx)?$': 'ts-jest',
-    '^.+\\.(js|jsx)$': 'babel-jest',
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": ["babel-jest", { "configFile": "./babel.config.jest.js" }],
   },
   transformIgnorePatterns: [
     "node_modules/(?!@auth)/"
