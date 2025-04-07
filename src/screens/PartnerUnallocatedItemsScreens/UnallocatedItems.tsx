75b7ebf09defac8fcc775a9ad6956c85fe44b0db
Liane Nguyen <lianenguyen@Lianes-Laptop.local>
2025-04-07 00:25:13 -0400

    liane/update-partner-request-item-modal

--- src/screens/PartnerUnallocatedItemsScreens/UnallocatedItems.tsx
+++ src/screens/PartnerUnallocatedItemsScreens/UnallocatedItems.tsx
@@ -141,7 +141,7 @@ function RequestItemsModal({
             {items.map((item, i) => (
               <tr key={JSON.stringify(item)}>
                 <td className="px-2">{item.title}</td>
-                <td className="px-2">{item.expirationDate}</td>
+                <td className="px-2">{item.expirationDate.split('T')[0]}</td>
                 <td className="px-2">
                   <div className="w-32">
                     <ModalDropDown
@@ -199,7 +199,7 @@ function RequestItemsModal({
             onClick={handleSubmit}
             className="block grow bg-red-500 text-center text-white py-1 px-4 rounded font-medium hover:bg-red-600 transition"
           >
-            Add item
+            Request items
           </button>
         </div>
       </div>
