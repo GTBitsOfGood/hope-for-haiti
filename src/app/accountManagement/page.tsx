8d6b0b0b289e837649e5614101b29f8e3cc8f8bb
Krish Prasad <krishkp00@gmail.com>
2025-09-09 10:56:27 -0400

    restyle table

--- src/app/accountManagement/page.tsx
+++ src/app/accountManagement/page.tsx
@@ -92,17 +92,20 @@ export default function AccountManagementPage() {
 
   return (
     <>
-      <h1 className="text-2xl font-semibold">Account Management</h1>
-      <div className="flex justify-between items-center w-full py-4">
+      <h1 className="text-3xl text-gray-primary font-bold">
+        Account Management
+      </h1>
+
+      <div className="flex justify-between items-center w-full pt-6">
         <div className="relative w-1/3">
           <MagnifyingGlass
-            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-primary/50"
             size={18}
           />
           <input
             type="text"
             placeholder="Search"
-            className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-lg bg-gray-100 focus:outline-none focus:border-gray-400"
+            className="pl-10 pr-4 py-2 text-gray-primary/50 w-full border border-gray-primary/10 rounded-lg bg-[#F9F9F9] focus:outline-none focus:border-gray-400"
           />
         </div>
         <button
@@ -112,7 +115,8 @@ export default function AccountManagementPage() {
           <Plus size={18} /> Add account
         </button>
       </div>
-      <div className="flex space-x-4 mt-4 border-b-2">
+
+      <div className="flex space-x-4 mt-3 border-b-2">
         {Object.keys(filterMap).map((tab) => {
           const key = tab as UserFilterKey;
 
@@ -120,10 +124,12 @@ export default function AccountManagementPage() {
             <button
               key={tab}
               data-active={activeTab === tab}
-              className="px-2 py-1 text-md font-medium relative -mb-px transition-colors focus:outline-none data-[active=true]:border-b-2 data-[active=true]:border-black data-[active=true]:bottom-[-1px] data-[active=false]:text-gray-500"
+              className="px-1 py-1 text-md font-medium relative -mb-px transition-colors focus:outline-none data-[active=true]:text-gray-primary data-[active=true]:border-b-2 data-[active=true]:border-gray-primary/70 data-[active=true]:border-black data-[active=true]:bottom-[-1px] data-[active=false]:text-gray-primary/70"
               onClick={() => filterUsers(key)}
             >
-              <div className="hover:bg-gray-100 px-2 py-1 rounded">{tab}</div>
+              <div className="hover:bg-gray-primary/5 px-2 py-1 rounded">
+                {tab}
+              </div>
             </button>
           );
         })}
@@ -134,17 +140,19 @@ export default function AccountManagementPage() {
           <CgSpinner className="w-16 h-16 animate-spin opacity-50" />
         </div>
       ) : (
-        <div className="overflow-x-scroll">
-          <table className="mt-4">
+        <div className="overflow-x-auto overflow-y-auto max-h-[72vh]">
+          <table className="mt-4 w-full">
             <thead>
-              <tr className="bg-gray-100 border-b-2 font-bold">
-                <th className="px-4 py-2 text-left w-1/5 rounded-tl-lg">
+              <tr className="bg-gray-primary/5 border-b-2 border-gray-primary/10 text-gray-primary/70">
+                <th className="px-4 py-4 text-left w-1/5 rounded-tl-lg font-normal">
                   Name
                 </th>
-                <th className="px-4 py-2 text-left w-1/5">Email</th>
-                <th className="px-4 py-2 text-left w-1/5">Role</th>
-                <th className="px-4 py-2 text-left w-1/5">Status</th>
-                <th className="px-4 py-2 text-left rounded-tr-lg w-1/12">
+                <th className="px-4 py-4 text-left w-1/5 font-normal">Email</th>
+                <th className="px-4 py-4 text-left w-1/5 font-normal">Role</th>
+                <th className="px-4 py-4 text-left w-1/5 font-normal">
+                  Status
+                </th>
+                <th className="px-4 py-4 text-left rounded-tr-lg w-1/12 font-normal">
                   Manage
                 </th>
               </tr>
--- src/components/AccountManagement/TableRow.tsx
+++ src/components/AccountManagement/TableRow.tsx
@@ -28,23 +28,23 @@ export default function TableRow({
     <tr
       key={index}
       data-odd={index % 2 !== 0}
-      className="bg-white data-[odd=true]:bg-gray-50"
+      className="bg-white data-[odd=false]:bg-sunken border-b border-gray-primary/10 text-gray-primary"
     >
-      <td className="border-b px-4 py-2">{user.name}</td>
-      <td className="border-b px-4 py-2">{user.email}</td>
-      <td className="border-b px-4 py-2">
+      <td className="border-b px-4 py-4">{user.name}</td>
+      <td className="border-b px-4 py-4">{user.email}</td>
+      <td className="border-b px-4 py-4">
         {formatUserType(user.type || user.userType!)}
       </td>
-      <td className="border-b px-4 py-2">
+      <td className="border-b px-4 py-4">
         <span
-          className={`px-2 py-1 rounded whitespace-nowrap ${
+          className={`px-3 py-2 rounded whitespace-nowrap ${
             isInvite ? "bg-yellow-primary" : "bg-green-primary"
           }`}
         >
           {isInvite ? "Pending" : "Account created"}
         </span>
       </td>
-      <td className="border-b px-4 py-2">
+      <td className="border-b px-4 py-4">
         <div className="float-right">
           <DotsThree
             weight="bold"
