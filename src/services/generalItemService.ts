cdf4bdc82adafbc017b0fdafa81af9aa041ad073
Renato Dell'Osso <rddellosso4@gmail.com>
2025-09-24 21:00:43 -0400

    Add GET /api/generalItems/unallocated

--- src/services/generalItemService.ts
+++ src/services/generalItemService.ts
@@ -7,4 +7,18 @@ export class GeneralItemService {
       data: item,
     });
   }
+
+  static async getUnallocatedItems() {
+    // A general item is unallocated if the sum of its allocated line items' quantities is less than its initial quantity
+    const items = await db.$queryRaw`
+      SELECT gi.*
+      FROM "GeneralItem" gi
+      INNER JOIN "LineItem" li
+      ON gi.id = li."generalItemId"
+      GROUP BY gi.id
+      HAVING SUM(li.quantity) < gi."initialQuantity"
+    `;
+
+    return items;
+  }
 }
