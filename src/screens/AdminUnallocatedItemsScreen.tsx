77718d389260f6a0665fad0a887ed85654bb75dd
Renato Dell'Osso <rddellosso4@gmail.com>
2025-10-08 21:16:09 -0400

    Use emptyState instead of isLoading

--- src/screens/AdminUnallocatedItemsScreen.tsx
+++ src/screens/AdminUnallocatedItemsScreen.tsx
@@ -49,7 +49,6 @@ export default function AdminUnallocatedItemsScreen() {
   const unitTypes = data?.unitTypes || [];
   const donorNames = data?.donorNames || [];
   const itemTypes = data?.itemTypes || [];
-  const isLoading = !data;
 
   const tableRef = useRef<AdvancedBaseTableHandle<UnallocatedItemData>>(null);
 
@@ -121,66 +120,65 @@ export default function AdminUnallocatedItemsScreen() {
         />
       ) : null}
       <h1 className="text-2xl font-semibold">Unallocated Items</h1>
-      <div
-        className={`${isLoading ? "" : "hidden"} flex justify-center items-center mt-8`}
-      >
-        <CgSpinner className="w-16 h-16 animate-spin opacity-50" />
-      </div>
-      <div className={isLoading ? "hidden" : ""}>
-        <AdvancedBaseTable
-          ref={tableRef}
-          columns={columns}
-          fetchFn={fetchTableData}
-          rowId="title"
-          pageSize={20}
-          toolBar={
-            <div className="flex justify-between items-center w-full">
-              <div className="relative w-1/3">
-                <MagnifyingGlass
-                  className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
-                  size={18}
-                />
-                <input
-                  type="text"
-                  placeholder="Search"
-                  className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-lg bg-gray-100 focus:outline-none focus:border-gray-400"
-                />
-              </div>
-              <div className="flex gap-4">
-                <div className="relative">
-                  <button
-                    className="flex items-center gap-2 bg-red-500 text-white px-4 py-2 rounded-lg font-medium hover:bg-red-600 transition"
-                    onClick={() => {
-                      setAddItemExpanded(!addItemExpanded);
-                    }}
-                  >
-                    <Plus size={18} /> Add Item
-                  </button>
-                  {addItemExpanded ? (
-                    <div className="flex flex-col gap-y-1 items-center absolute left-0 mt-2 p-1 origin-top-right bg-white border border-solid border-gray-primary rounded border-opacity-10">
-                      <button
-                        className="block font-medium w-full rounded text-gray-primary text-opacity-70 text-center px-2 py-1 hover:bg-gray-primary hover:bg-opacity-5"
-                        onClick={() => {
-                          setIsModalOpen(true);
-                          setAddItemExpanded(false);
-                        }}
-                      >
-                        Single item
-                      </button>
-                      <button
-                        className="block font-medium w-full rounded text-gray-primary text-opacity-70 text-center px-2 py-1 hover:bg-gray-primary hover:bg-opacity-5"
-                        onClick={() => router.push("/bulkAddItems")}
-                      >
-                        Bulk items
-                      </button>
-                    </div>
-                  ) : null}
-                </div>
+
+      <AdvancedBaseTable
+        ref={tableRef}
+        columns={columns}
+        fetchFn={fetchTableData}
+        rowId="title"
+        pageSize={20}
+        emptyState={
+          <div className="flex justify-center items-center mt-8">
+            <CgSpinner className="w-16 h-16 animate-spin opacity-50" />
+          </div>
+        }
+        toolBar={
+          <div className="flex justify-between items-center w-full">
+            <div className="relative w-1/3">
+              <MagnifyingGlass
+                className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500"
+                size={18}
+              />
+              <input
+                type="text"
+                placeholder="Search"
+                className="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-lg bg-gray-100 focus:outline-none focus:border-gray-400"
+              />
+            </div>
+            <div className="flex gap-4">
+              <div className="relative">
+                <button
+                  className="flex items-center gap-2 bg-red-500 text-white px-4 py-2 rounded-lg font-medium hover:bg-red-600 transition"
+                  onClick={() => {
+                    setAddItemExpanded(!addItemExpanded);
+                  }}
+                >
+                  <Plus size={18} /> Add Item
+                </button>
+                {addItemExpanded ? (
+                  <div className="flex flex-col gap-y-1 items-center absolute left-0 mt-2 p-1 origin-top-right bg-white border border-solid border-gray-primary rounded border-opacity-10">
+                    <button
+                      className="block font-medium w-full rounded text-gray-primary text-opacity-70 text-center px-2 py-1 hover:bg-gray-primary hover:bg-opacity-5"
+                      onClick={() => {
+                        setIsModalOpen(true);
+                        setAddItemExpanded(false);
+                      }}
+                    >
+                      Single item
+                    </button>
+                    <button
+                      className="block font-medium w-full rounded text-gray-primary text-opacity-70 text-center px-2 py-1 hover:bg-gray-primary hover:bg-opacity-5"
+                      onClick={() => router.push("/bulkAddItems")}
+                    >
+                      Bulk items
+                    </button>
+                  </div>
+                ) : null}
               </div>
             </div>
-          }
-        />
-      </div>
+          </div>
+        }
+      />
     </>
   );
 }
