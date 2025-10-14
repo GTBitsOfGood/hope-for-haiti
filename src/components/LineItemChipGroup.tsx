2b63704b21864b430fb9c7ff2a4225977c7168d8
Renato Dell'Osso <rddellosso4@gmail.com>
2025-10-13 22:33:00 -0400

    Include corner minichip in button

--- src/components/LineItemChipGroup.tsx
+++ src/components/LineItemChipGroup.tsx
@@ -142,18 +142,18 @@ function LineItemChip({
     <div className="relative">
       <button
         onClick={() => setIsDropdownOpen(!isDropdownOpen)}
-        className="rounded-lg border border-blue-300 bg-white m-2 px-2 py-1 text-sm font-bold flex items-center gap-1 hover:shadow"
+        className="relative rounded-lg border border-blue-300 bg-white m-2 px-2 py-1 text-sm font-bold flex items-center gap-1 hover:shadow"
       >
         <span>{item.palletNumber}</span>
         <span className="rounded-lg bg-blue-200 text-blue-500 px-1">
           {item.quantity}
         </span>
+        <span
+          className={`absolute -left-2 -top-2 rounded text-xs px-1 shadow-sm ${item.allocation ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-500"}`}
+        >
+          {item.allocation?.partner ? item.allocation.partner.name : "None"}
+        </span>
       </button>
-      <span
-        className={`absolute left-0 top-0 rounded text-xs px-1 shadow-sm ${item.allocation ? "bg-blue-500 text-white" : "bg-gray-200 text-gray-500"}`}
-      >
-        {item.allocation?.partner ? item.allocation.partner.name : "None"}
-      </span>
 
       {/* Allocation Dropdown */}
       <div
