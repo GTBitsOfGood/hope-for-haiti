c62ace271302b09c8ede282371b60124c4cf09f4
Renato Dell'Osso <rddellosso4@gmail.com>
2025-11-07 22:39:33 -0500

    Better message disabling

--- src/components/tickets/TicketMessageInput.tsx
+++ src/components/tickets/TicketMessageInput.tsx
@@ -1,10 +1,27 @@
 import { ExtraChannelData } from "@/types/api/streamio.types";
+import { useEffect } from "react";
 import toast from "react-hot-toast";
-import { MessageInput, useChannelStateContext } from "stream-chat-react";
+import {
+  MessageInput,
+  useChannelStateContext,
+  useMessageComposer,
+} from "stream-chat-react";
 
 export default function TicketMessageInput() {
   const { channel } = useChannelStateContext();
+  const composer = useMessageComposer();
+
   const data = channel.data as ExtraChannelData;
+
+  useEffect(() => {
+    // Disable message input if the ticket is closed
+    composer.config.text.enabled = !data.closed;
+    composer.config.drafts.enabled = !data.closed;
+    composer.config.location.enabled = !data.closed;
+    composer.config.attachments.maxNumberOfFilesPerMessage = data.closed
+      ? 0 // Will disable attachments
+      : 10; // Default is 10
+  }, [data.closed, composer.config]);
 
   return (
     <MessageInput
