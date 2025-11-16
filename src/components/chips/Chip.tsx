439d9d555138e887c302cba7c5cff39843181b1c
Sujal Dhakal <sujaldhakal06@gmail.com>
2025-11-16 10:02:04 -0500

    added rendering conditional

--- src/components/chips/Chip.tsx
+++ src/components/chips/Chip.tsx
@@ -42,7 +42,7 @@ export default function Chip({
         className={`relative rounded-lg border border-blue-primary m-2 px-2 py-1 text-sm flex items-center gap-1 hover:shadow ${className}`}
       >
         <span className="text-blue-primary">{title}</span>
-        {revisedAmount !== amount && (
+        {amount !== undefined && revisedAmount !== amount && (
           <span className="line-through text-gray-primary/50 font-bold px-[2px]">
             {amount}
           </span>
